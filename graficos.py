4c7e903892466028f40dead2001cb53c499bddcb
Samarah Ramos <samarahluiza1250@gmail.com>
2025-06-04 10:31:54 -0300

    Consegui plotar com o plt

--- graficos.py
+++ graficos.py
@@ -1,1 +1,12 @@
 import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+plt.plot(x, y, marker='o')
+
+plt.title('Exemplo de Gráfico de Linha')
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+
+plt.show()
