fd6599e1f09e7d8fc1bd3675e7c6ca2b76da66ee
lazaroribeiro <lazarost.ribeiro@gmail.com>
2025-06-11 10:51:11 -0300

    Criei o sistema que permite com que o usuário consiga quantos pontos terão o  gráfico;

--- arquivos.py
+++ arquivos.py
@@ -1,23 +1,24 @@
 import pandas as pd
 
-pressao = float(input())
-volume = float(input())
-temperatura = float(input())
-def salvar_arquivo(pressao, volume, temperatura):
-    # Cria um DataFrame com os dados
-    df = pd.DataFrame({
-        'Pressão (P)': [pressao],
-        'Volume (V)': [volume],
-        'Temperatura (T)': [temperatura]
-    })
-    
-    # Salva o DataFrame em um arquivo CSV
-    df.to_csv('dados_termodinamicos.csv', index=False)
-    print("Dados salvos no arquivo 'dados_termodinamicos.csv'.")
+qantidade_de_pontos = int(input("Digite a quantidade de pontos: "))
 
-teste = salvar_arquivo(pressao, volume, temperatura)
-print(teste)
+dados = { 'Pressão (P)': [],
+         'Volume (V)': [],      
+         'Temperatura (T)': [] }
+# Coleta os dados de pressão, volume e temperatura
 
-arquivo_lido = pd.read_csv('dados_termodinamicos.csv')
-arquivo_lido.to_excel('dados_termodinamicos.xlsx', index=None, header=True)
-                 
+for _ in range(qantidade_de_pontos):
+    pressao = float(input("digite a pressão: "))
+    volume = float(input("digite o volume: "))
+    temperatura = float(input("digite a temperatura: "))
+    dados['Pressão (P)'].append(pressao)
+    dados['Volume (V)'].append(volume)  
+    dados['Temperatura (T)'].append(temperatura)
+
+df = pd.DataFrame(dados)
+
+df.to_csv('dados.csv', index=False)
+
+arquivo_lido = pd.read_csv('dados.csv')
+arquivo_lido = pd.to_excel('dados.xlsx', index=False)
+print("Dados salvos com sucesso!")
