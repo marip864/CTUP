cd5856ababf1a2aa23e10e054b4eba6c332d0599
Caio1833 <caioavilapaulo@gmail.com>
2025-06-11 11:41:55 -0300

    Arredondei. Mudei o output do rendimento.

--- calculos.py
+++ calculos.py
@@ -2,7 +2,8 @@ teste = ((10, 2, 5),(20, 1, 5), (20, 2, 10), (40, 1, 10), (10, 2, 5))
 #Recebe os dados do input como uma tupla de tuplas (P, V, T)
 from math import log
 def calculadora(tupla):
-    transformações = []
+    transformações = [] #Aqui ficam os valores reais, para aproximar no final
+    transformações_bonitas = [] #Aqui ficam os valores aproximados
     for n in range(len(tupla)-1):
         pontoA = tupla[n]
         pontoB = tupla[n+1]
@@ -17,7 +18,6 @@ def calculadora(tupla):
         UAB = (3/2) * ((PB * VB) - (PA * VA))
 #Isotérmica:
         if TA == TB:
-        #if PA * VA == PB * VB:(problemas com operações de float)
             WAB = PA * VA * log(VB / VA)
             SAB = ((PA * VA)/TA) * log(VB/VA)
 #Isobárica:
@@ -30,14 +30,14 @@ def calculadora(tupla):
             SAB = (3/2) * ((PA * VA)/TA) * log(TB/TA) 
 #Adiabática:
         else:
-        #elif PA * (VA**(5/3)) == PB * (VB**(5/3)):(problemas com operações de float)
             WAB = -UAB
             SAB = 0
         QAB = WAB + UAB
         transformações.append((QAB, WAB, UAB, SAB))
+        transformações_bonitas.append((round(QAB,3), round(WAB,3), round(UAB, 3), round(SAB, 3)))
 #Lista Q, W e delta U de cada transformação em ordem, na forma de tuplas.
-#Agora, calcula Qrecebido, Qcedido, W, delta U e delta S totais e rendimento e adiciona por último na lista.
-# Depois transforma em tupla. Se não for um ciclo, o rendimento é None.
+#Agora, calcula Qrecebido, Qcedido, W, delta U e delta S totais.
+# Depois arredonda e adiciona por último na lista.
     Qr = 0
     Qc = 0
     W = 0
@@ -51,12 +51,13 @@ def calculadora(tupla):
         W += transformações[t][1]
         U += transformações[t][2]
         S += transformações[t][3]
+    total_bonito = (round(Qr,3),round(Qc,3),round(W,3),round(U,3),round(S,3))
+    transformações_bonitas.append(total_bonito)
+#Se for um ciclo, calcula o rendimento aproximado.
     if teste[0] == teste[-1]:
         rendimento = W/Qr
-    else:
-        rendimento = None
-    total = (Qr,Qc, W, U, S, rendimento)
-    transformações.append(total)
-    transformações = tuple(transformações)
-    return transformações
+        transformações_bonitas.append(round(rendimento, 3))
+#Agora transforma os dados na tupla a ser retornada.
+    transformações_bonitas = tuple(transformações_bonitas)
+    return transformações_bonitas
 print(calculadora(teste))
