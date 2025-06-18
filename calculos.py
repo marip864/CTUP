ca78c84140defc7756a9ebc2f8e15839c2db0d1c
Caio1833 <caioavilapaulo@gmail.com>
2025-06-18 08:28:07 -0300

    Comentei melhor

--- calculos.py
+++ calculos.py
@@ -3,7 +3,7 @@ teste = ((10, 2, 5),(20, 1, 5), (20, 2, 10), (40, 1, 10), (10, 2, 5))
 from math import log
 def calculadora(tupla):
     transformações = [] #Aqui ficam os valores reais, para aproximar no final
-    transformações_bonitas = [] #Aqui ficam os valores aproximados
+    transformações_arredondadas = [] #Aqui ficam os valores aproximados
     for n in range(len(tupla)-1):
         pontoA = tupla[n]
         pontoB = tupla[n+1]
@@ -14,7 +14,7 @@ def calculadora(tupla):
         VB = pontoB[1]
         TB = pontoB[2]
 #Verifica o tipo de transformação e calcula delta U, delta S, W e Q para cada uma delas.
-#Se nenhum input for igual, assume que é adiabática.
+#Se nenhum input for igual, assume que é adiabática reversível.
         UAB = (3/2) * ((PB * VB) - (PA * VA))
 #Isotérmica:
         if TA == TB:
@@ -33,11 +33,10 @@ def calculadora(tupla):
             WAB = -UAB
             SAB = 0
         QAB = WAB + UAB
+#Lista Q, W e delta U de cada transformação em ordem, na forma de tuplas, e arredonda.
         transformações.append((QAB, WAB, UAB, SAB))
-        transformações_bonitas.append((round(QAB,3), round(WAB,3), round(UAB, 3), round(SAB, 3)))
-#Lista Q, W e delta U de cada transformação em ordem, na forma de tuplas.
-#Agora, calcula Qrecebido, Qcedido, Q, W, delta U e delta S totais.
-#Depois arredonda e adiciona por último na lista.
+        transformações_arredondadas.append((round(QAB,3), round(WAB,3), round(UAB, 3), round(SAB, 3)))
+#Agora, calcula Qrecebido, Qcedido, Q, W, delta U e delta S totais, arredonda e adiciona por último.
     Qr = 0
     Qc = 0
     W = 0
@@ -52,13 +51,13 @@ def calculadora(tupla):
         W += transformações[t][1]
         U += transformações[t][2]
         S += transformações[t][3]
-    total_bonito = (round(Qr,3),round(Qc,3),round(Q,3), round(W,3),round(U,3),round(S,3))
-    transformações_bonitas.append(total_bonito)
+    total_arredondado = (round(Qr,3),round(Qc,3),round(Q,3), round(W,3),round(U,3),round(S,3))
+    transformações_arredondadas.append(total_arredondado)
 #Se for um ciclo, calcula o rendimento aproximado.
     if teste[0] == teste[-1]:
         rendimento = W/Qr
-        transformações_bonitas.append(round(rendimento, 3))
+        transformações_arredondadas.append(round(rendimento, 3))
 #Agora transforma os dados na tupla a ser retornada.
-    transformações_bonitas = tuple(transformações_bonitas)
-    return transformações_bonitas
+    transformações_arredondadas = tuple(transformações_arredondadas)
+    return transformações_arredondadas
 print(calculadora(teste))
